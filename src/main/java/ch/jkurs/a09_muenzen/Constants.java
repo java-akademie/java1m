6db51daf34faf579d4edad7f33cc82b95ff54dfb
jmildner <johann.mildner@balcab.ch>
2016-06-29 10:20:37 +0200

    jmb

--- src/main/java/ch/jkurs/a09_muenzen/Constants.java
+++ src/main/java/ch/jkurs/a09_muenzen/Constants.java
@@ -4,11 +4,11 @@ import java.text.SimpleDateFormat;
 
 abstract class Constants
 {
-	static final int SUM_WEIGHT = 379644;
-	static final int SUM_AMOUNT = 22198;
+	//static final int SUM_WEIGHT = 379644;
+	//static final int SUM_AMOUNT = 22198;
 
-	// static final int SUM_WEIGHT = 200000; // 2 kg
-	// static final int SUM_AMOUNT = 20000; // 200 €
+	 static final int SUM_WEIGHT = 200000; // 2 kg
+	 static final int SUM_AMOUNT = 20000; // 200 €
 
 	static final SimpleDateFormat DF = new SimpleDateFormat(
 			"dd.MM.yyyy HH:mm:ss");
