e5d55ac3e946e728f8bb94700dcf6bca6dd91a42
jmildner <johann.mildner@balcab.ch>
2016-11-05 10:05:34 +0100

    jmb

--- src/main/java/ch/java_akademie/message/Messages.java
+++ src/main/java/ch/java_akademie/message/Messages.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public class Messages
 {
-	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "messages"; 
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
