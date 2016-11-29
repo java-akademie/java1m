24f3f8c9e6a3e00dee66e563c8bf08db7b5c084c
jmildner <johann.mildner@balcab.ch>
2016-11-29 11:38:23 +0100

    jmb

---
+++ src/main/java/ch/jkurs/a13_wahrscheinlichkeit/GeburtstagAm29_02.java
@@ -0,0 +1,55 @@
+package ch.jkurs.a13_wahrscheinlichkeit;
+
+
+
+import java.util.Date;
+
+import ch.java_akademie.tools.DateTime;
+import ch.java_akademie.tools.MyTools;
+
+public class GeburtstagAm29_02
+{
+	final static int MAX_PERSONEN = 1000000;
+	final static int JAHR = 2001;
+	final static int TAGE = 365;
+
+
+	public static void main(String[] args) throws Exception
+	{
+		empirisch();
+		mathematisch();
+	}
+
+
+	private static void mathematisch() throws Exception
+	{
+		MyTools.uebOut("Mathematisch", 2);
+		double guenstig = 1, moeglich = TAGE;
+		double w = guenstig / moeglich;
+
+		System.out.printf(
+				"Wahrscheinlichkeit dass der Geburtstag an einem bestimmten Tag ist: %f %n", w);
+	}
+
+
+	private static void empirisch() throws Exception
+	{
+		MyTools.uebOut("Empirisch", 2);
+
+		Date bestimmterTag = DateTime.makeRandomDate(JAHR);
+
+		int anzahl = 0;
+
+		for (int i = 1; i <= MAX_PERSONEN; i++)
+		{
+			Date d = DateTime.makeRandomDate(JAHR);
+			if (d.equals(bestimmterTag))
+				anzahl++;
+		}
+
+		System.out.printf("Anzahl von %,d Geburtstagen am %s  ist  %d  %n", MAX_PERSONEN,
+				bestimmterTag, anzahl);
+		System.out.printf("Durchschnitt %f %n", (double) anzahl / MAX_PERSONEN);
+	}
+
+}
---
+++ src/main/java/ch/jkurs/a13_wahrscheinlichkeit/GleicherGeburtstag.java
@@ -0,0 +1,92 @@
+package ch.jkurs.a13_wahrscheinlichkeit;
+
+
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import ch.java_akademie.tools.DateTime;
+import ch.java_akademie.tools.MyTools;
+
+public class GleicherGeburtstag
+{
+	final static int MAX_DURCHLAEUFE = 100000;
+	final static int MAX_SCHUELER = 28;
+	final static int JAHR = 2001;
+	final static int TAGE = 365;
+
+
+	public static void main(String[] args) throws Exception
+	{
+		empirisch();
+		mathematisch();
+	}
+
+
+	private static void mathematisch() throws Exception
+	{
+		MyTools.uebOut("Mathematisch", 2);
+
+		double ps = 1.0;
+
+		for (int i = 0; i < MAX_SCHUELER; i++)
+		{
+			double t = TAGE - i;
+			ps *= t / TAGE;
+		}
+
+		System.out.printf("Gegenwahrscheinlichkeit dass keiner am selben Tag Geburtstag hat: %f %n",
+				1 - ps);
+	}
+
+
+	private static void empirisch() throws Exception
+	{
+		MyTools.uebOut("Empirisch", 2);
+
+		int gesamt = 0;
+		int klassen = 0;
+
+		for (int i = 1; i <= MAX_DURCHLAEUFE; i++)
+		{
+			int gleiche = doit(MAX_SCHUELER, JAHR);
+			gesamt += doit(MAX_SCHUELER, JAHR);
+			klassen += gleiche == 0 ? 0 : 1;
+		}
+
+		System.out.printf("Durchschnitt Anzahl Schueler mit doppelten Geburtstagen: %6.1f %n",
+				(double) gesamt / MAX_DURCHLAEUFE);
+		System.out.printf("Durchschnitt Anzahl Klassen  mit doppelten Geburtstagen: %8.3f %n",
+				(double) klassen / MAX_DURCHLAEUFE);
+	}
+
+
+	private static int doit(final int MAX_SCHUELER, final int JAHR) throws Exception
+	{
+		Date[] d = new Date[MAX_SCHUELER];
+
+		for (int i = 0; i < MAX_SCHUELER; i++)
+		{
+			d[i] = DateTime.makeRandomDate(JAHR);
+		}
+
+		// es gibt jetzt MAX Daten
+		// die werden in ein Set gestellt
+		// ist das Set kleiner MAX gibt es doppelte Geburtstage
+
+		Set<Date> verschiedeneGeburtstage = new HashSet<Date>();
+
+		for (int i = 0; i < MAX_SCHUELER; i++)
+		{
+			verschiedeneGeburtstage.add(d[i]);
+		}
+
+		int gleich = MAX_SCHUELER - verschiedeneGeburtstage.size();
+
+		// System.out.printf("gleiche Geburtstage: %d %n", gleich);
+
+		return gleich;
+	}
+
+}
