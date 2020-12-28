7761c591a4985fa2e93070a1ef74ebd4434c7d2b
Johann Mildner <johann.mildner@balcab.ch>
2020-12-28 11:59:20 +0100

    jmb 28.12.2020

--- src/main/java/ch/java_akademie/applet/MyFrame1.java
+++ src/main/java/ch/java_akademie/applet/MyFrame1.java
@@ -4,13 +4,12 @@ import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class MyFrame1
-{
+public class MyFrame1 {
 	Frame frame;
 
+	//...
 
-	public MyFrame1(String feld)
-	{
+	public MyFrame1(String feld) {
 
 		frame = new Frame("Frame");
 
@@ -21,19 +20,15 @@ public class MyFrame1
 		frame.pack();
 		frame.setVisible(true);
 
-		frame.addWindowListener(new WindowAdapter()
-		{
-			public void windowClosing(WindowEvent e)
-			{
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
 				System.out.println("Window geschlossen" + e);
 				System.exit(0);
 			}
 		});
 	}
 
-
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("start main");
 		new MyFrame1("mein Frame");
 	}
